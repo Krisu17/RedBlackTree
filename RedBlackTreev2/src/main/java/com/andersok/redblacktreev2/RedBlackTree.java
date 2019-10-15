e9531be6f47371543de3f825c386ea4c6d949658
Krisu17 <48809984+Krisu17@users.noreply.github.com>
2019-10-15 11:19:07 +0200

    Napisane rebuildTree

--- RedBlackTreev2/src/main/java/com/andersok/redblacktreev2/RedBlackTree.java
+++ RedBlackTreev2/src/main/java/com/andersok/redblacktreev2/RedBlackTree.java
@@ -11,31 +11,76 @@ public class RedBlackTree implements MapInterface {
         nodes = new ArrayList<>();
         head = null;
     }
-    
+
     private void leftRotation(Node x) {
-        
+
     }
-    
+
     private void rightRotation(Node x) {
-        
+
     }
-    
-    private void rebuildTree() {
-        
+
+    private void rebuildTree(Node x, Node grandfather) {
+        if (grandfather != null) {
+            if (grandfather.getLeft() == x.getUp()) { //ojciec jest lewym synem swojego ojca
+                if (grandfather.getRight().getColor() == 'r') {
+                    grandfather.getLeft().setColor('b');
+                    grandfather.getRight().setColor('b');
+                    grandfather.setColor('r');
+                } else if (grandfather.getRight().getColor() == 'b' 
+                        && grandfather.getLeft().getRight() == x) {
+                    leftRotation(x.getUp());
+                } else {
+                    x.getUp().setColor('r');
+                    grandfather.setColor('r');
+                    rightRotation(grandfather);
+                }
+            }
+        }
     }
 
     @Override
     public void setValue(Comparable key, Object value) {
-        if(head == null) {
+        if (head == null) {
             head = new Node(key, value);
             head.setUp(null);
             head.setLeft(null);
             head.setRight(null);
             head.setColor('b');
-        }
-        else {
+        } else {
             Node newFather = head;
-            
+            Node grandfather = null;
+            boolean flag = true;
+            while (flag) {
+                if (key.compareTo(newFather.getKey()) < 0) {
+                    if (newFather.getLeft() != null) {
+                        grandfather = newFather;
+                        newFather = newFather.getLeft();
+                    } else {
+                        Node n = new Node(key, value);
+                        n.setUp(newFather);
+                        newFather.setLeft(n);
+                        n.setLeft(null);
+                        n.setRight(null);
+                        n.setColor('r');
+                        flag = false;
+                        rebuildTree(n, grandfather);
+                    }
+                } else {
+                    if (newFather.getRight() != null) {
+                        newFather = newFather.getRight();
+                    } else {
+                        Node n = new Node(key, value);
+                        n.setUp(newFather);
+                        newFather.setRight(n);
+                        n.setLeft(null);
+                        n.setRight(null);
+                        n.setColor('r');
+                        flag = false;
+                    }
+                }
+            }
+
         }
     }
 
